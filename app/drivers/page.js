1b4ac43b0e92b37ca1853382c6e0e590ba074290
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-08 21:45:06 +0200

    adding drivers number

--- app/drivers/page.js
+++ app/drivers/page.js
@@ -10,6 +10,7 @@ export default function Drivers() {
       .then((response) => response.json())
       .then((data) => {
         setDrivers(data);
+        console.log(data);
       })
       .catch((error) => console.error("Error fetching driver data:", error));
   }, []);
@@ -28,19 +29,22 @@ export default function Drivers() {
         {drivers.map((driver) => (
           <div
             key={driver.driver_number}
-            className={` rounded-tr-3xl p-4 flex flex-col items-center justify-center shadow-md`}
+            className={` rounded-tr-3xl p-4 flex justify-evenly shadow-md`}
             style={{
               borderTop: `10px solid #${driver.team_colour}`,
               borderRight: `10px solid #${driver.team_colour}`,
             }}
           >
-            <img
-              src={driver.headshot_url}
-              alt={`${driver.full_name} headshot`}
-              className="w-24 h-24 rounded-full mb-4"
-            />
-            <h2 className="text-lg font-semibold mb-2">{driver.full_name}</h2>
-            <p className="text-sm text-gray-800">Team: {driver.team_name}</p>
+            <p className="text-4xl">{driver.driver_number}</p>
+            <div className="flex-col items-center justify-center">
+              <img
+                src={driver.headshot_url}
+                alt={`${driver.full_name} headshot`}
+                className="w-24 h-24  mb-4"
+              />
+              <h2 className="text-lg font-semibold mb-2">{driver.full_name}</h2>
+              <p className="text-sm text-gray-800">Team: {driver.team_name}</p>
+            </div>
           </div>
         ))}
       </div>
