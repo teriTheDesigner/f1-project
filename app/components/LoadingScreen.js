380e55feeb31812df10c085058dc396441eb6f03
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-15 17:55:15 +0200

    loading screen animation

---
+++ app/components/LoadingScreen.js
@@ -0,0 +1,8 @@
+export default function LoadingScreen() {
+  return (
+    <div className="loadingContainer">
+      <h1 className="text-3xl">Welcome to F1</h1>
+      <img src="/f1-car.png" alt="f1 car" className="image" />
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,4 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import LoadingScreen from "../app/components/LoadingScreen";
+
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1600);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <main>
       <h1 className="text-7xl mt-10 font-extrabold">Helloo to all F1 Fans!</h1>
