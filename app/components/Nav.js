7e89dc4e0bb59716c07d0f6850a5870a957bc3cf
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-15 18:47:05 +0200

    mobile navigation

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,14 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="bg-[#E00400] w-full h-16 flex justify-center items-center text-white ">
-      <nav className="flex justify-center items-center gap-10">
-        <Link href="/">
+    <div>
+      <div className="bg-[#E00400]  w-full h-10 md:hidden flex justify-between items-center text-white ">
+        <Link href="/" className="ml-4">
           <Image
             src="/logof1.webp"
             alt="Formula 1 Logo"
@@ -13,13 +16,57 @@ export default function Nav() {
             height={100}
           />
         </Link>
-        <Link href="/schedule" className="active:border active:">
-          Schedule
-        </Link>
-        <Link href="/2024season">2024 Season</Link>
-        <Link href="/teams">Teams</Link>
-        <Link href="/drivers">Drivers</Link>
-      </nav>
+
+        <button
+          className="md:hidden block focus:outline-none"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className="space-y-2 mr-4">
+            <span className="block w-8 h-0.5 bg-white"></span>
+            <span className="block w-8 h-0.5 bg-white"></span>
+            <span className="block w-8 h-0.5 bg-white"></span>
+          </div>
+        </button>
+        <nav
+          className={`${
+            isOpen ? "block" : "hidden"
+          } md:flex md:items-center md:gap-10 absolute md:static top-10 left-0 w-full md:w-auto bg-[#E00400] md:bg-transparent p-5 md:p-0`}
+        >
+          <Link href="/schedule" className="block md:inline-block py-2 md:py-0">
+            Schedule
+          </Link>
+          <Link
+            href="/2024season"
+            className="block md:inline-block py-2 md:py-0"
+          >
+            2024 Season
+          </Link>
+          <Link href="/teams" className="block md:inline-block py-2 md:py-0">
+            Teams
+          </Link>
+          <Link href="/drivers" className="block md:inline-block py-2 md:py-0">
+            Drivers
+          </Link>
+        </nav>
+      </div>
+      <div className="bg-[#E00400] hidden w-full h-16 md:flex justify-center items-center text-white ">
+        <nav className="flex justify-center items-center gap-10">
+          <Link href="/">
+            <Image
+              src="/logof1.webp"
+              alt="Formula 1 Logo"
+              width={100}
+              height={100}
+            />
+          </Link>
+          <Link href="/schedule" className="active:border active:">
+            Schedule
+          </Link>
+          <Link href="/2024season">2024 Season</Link>
+          <Link href="/teams">Teams</Link>
+          <Link href="/drivers">Drivers</Link>
+        </nav>
+      </div>
     </div>
   );
 }
