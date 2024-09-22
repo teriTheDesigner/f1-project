410b36ecf4913bee078836785147979c5a28587d
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-22 19:40:35 +0200

    fetching data for schedule

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -1,3 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Schedule() {
-  return <div>Schedule page</div>;
+  const [sessions, setSessions] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.openf1.org/v1/sessions")
+      .then((response) => response.json())
+      .then((data) => {
+        setSessions(data);
+        console.log(data);
+      })
+      .catch((error) => console.error("Error fetching sessions data:", error));
+  }, []);
+
+  const groupSessionsByCircuit = (sessions) => {
+    return sessions.reduce((acc, session) => {
+      const { circuit_key, circuit_short_name } = session;
+      if (!acc[circuit_key]) {
+        acc[circuit_key] = {
+          circuit_short_name,
+          sessions: [],
+        };
+      }
+      acc[circuit_key].sessions.push(session);
+      return acc;
+    }, {});
+  };
+
+  // Grouping the sessions
+  const groupedSessions = groupSessionsByCircuit(sessions);
+
+  return (
+    <div>
+      <h1>Schedule 2024</h1>
+      {Object.keys(groupedSessions).map((circuitKey) => (
+        <div key={circuitKey} style={{ marginBottom: "20px" }}>
+          <h2>{groupedSessions[circuitKey].circuit_short_name} </h2>
+          <ul>
+            {groupedSessions[circuitKey].sessions.map((session) => (
+              <li key={session.session_key}>{session.session_name}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
