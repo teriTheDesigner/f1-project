1903327bd66f7a1db8d98217f009deee12901f70
teriTheDesigner <tere1109@stud.kea.dk>
2024-09-25 16:58:02 +0200

    displaying images based on location

--- app/schedule/page.js
+++ app/schedule/page.js
@@ -5,6 +5,105 @@ import { useEffect, useState } from "react";
 export default function Schedule() {
   const [sessions, setSessions] = useState([]);
 
+  const locations = [
+    {
+      location: "Silverstone",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Budapest",
+      img: "/LasVegas.jpg",
+    },
+    {
+      location: "Imola",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Spa-Francorchamps",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Melbourne",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Barcelona",
+      img: "/LasVegas.jpg",
+    },
+    {
+      location: "Spielberg",
+      img: "/LasVegas.jpg",
+    },
+    {
+      location: "Monaco",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Montréal",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Monza",
+      img: "/LasVegas.jpg",
+    },
+    {
+      location: "Suzuka",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Shanghai",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Zandvoort",
+      img: "/LasVegas.jpg",
+    },
+    {
+      location: "Marina Bay",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Sakhir",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Baku",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Jeddah",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Miami",
+      img: "/LasVegas.jpg",
+    },
+    {
+      location: "Austin",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "São Paulo",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Mexico City",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Yas Island",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Lusail",
+      img: "/UAE.jpg",
+    },
+    {
+      location: "Las Vegas",
+      img: "/LasVegas.jpg",
+    },
+  ];
+
   useEffect(() => {
     fetch("https://api.openf1.org/v1/sessions")
       .then((response) => response.json())
@@ -40,6 +139,10 @@ export default function Schedule() {
   };
 
   const groupedSessions = groupSessionsByYearAndCircuit(sessions);
+  const getImageForLocation = (location) => {
+    const locationObj = locations.find((loc) => loc.location === location);
+    return locationObj ? locationObj.img : "/default.jpg";
+  };
 
   return (
     <div>
@@ -48,14 +151,21 @@ export default function Schedule() {
       {Object.keys(groupedSessions)
         .sort((a, b) => b - a)
         .map((year) => (
-          <div key={year} style={{ marginBottom: "40px" }}>
-            <h2>{year}</h2>
+          <div key={year} className="grid grid-cols-3 gap-4 mb-10">
+            <h2 className="col-span-3 text-xl font-bold">{year}</h2>
 
             {Object.keys(groupedSessions[year]).map((circuitKey) => (
-              <div key={circuitKey} style={{ marginLeft: "20px" }}>
+              <div key={circuitKey} className="border p-4 rounded shadow-sm">
+                <img
+                  src={getImageForLocation(
+                    groupedSessions[year][circuitKey].location
+                  )}
+                  alt={`Image of ${groupedSessions[year][circuitKey].location}`}
+                  className="w-full h-auto"
+                />
                 <h3>{groupedSessions[year][circuitKey].country_name} </h3>
                 <h4>{groupedSessions[year][circuitKey].location} </h4>
-                <div style={{ marginBottom: "40px" }}>
+                {/* <div style={{ marginBottom: "40px" }}>
                   {groupedSessions[year][circuitKey].sessions
                     .sort(
                       (a, b) => new Date(a.date_start) - new Date(b.date_start)
@@ -65,7 +175,18 @@ export default function Schedule() {
                         {session.session_name}
                       </p>
                     ))}
-                </div>
+                </div> */}
+                {/* <div style={{ marginBottom: "40px" }}>
+                  {groupedSessions[year][circuitKey].sessions
+                    .sort(
+                      (a, b) => new Date(a.date_start) - new Date(b.date_start)
+                    )
+                    .map((session) => (
+                      <p className="titillium-web" key={session.session_key}>
+                        {session.session_name}
+                      </p>
+                    ))}
+                </div> */}
               </div>
             ))}
           </div>
